963e6a6fb7cbcafa1d9eedf5685f6e916a3e5d56
ericjiji3 <eric.ji3333@gmail.com>
2025-01-27 17:37:12 -0500

    einre

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-import ClientButton from '@/../public/images/client-button.png';
+import About from '@/../public/images/about-icon.png';
+import Contact from '@/../public/images/contact-icon.png';
+import Work from '@/../public/images/work-icon.png';
+import Client from '@/../public/images/clients-icon.png';
 
 
 export default function Home() {
@@ -10,28 +13,28 @@ export default function Home() {
       <div className="links-container">
           <Link href="/clients"  className="link-container">
             <div className="image-container">
-              <Image src={ClientButton} width={285} height={285} alt="oops"/>
+              <Image src={Client} width={285} height={285} alt="oops"/>
             </div>
             <span>CLIENTS</span>
           </Link>
 
           <Link href="/contact" className="link-container">
             <div className="image-container">
-              <Image src={ClientButton} width={285} height={285} alt="oops"/>
+              <Image src={Contact} width={285} height={285} alt="oops"/>
             </div>
             <span>CONTACT</span>
           </Link>
 
           <Link href="/work" className="link-container">
             <div className="image-container">
-              <Image src={ClientButton} width={285} height={285} alt="oops"/>
+              <Image src={Work} width={250} height={250} alt="oops"/>
             </div>
             <span>WORK</span>
           </Link>
 
           <Link href="/about" className="link-container">
             <div className="image-container">
-              <Image src={ClientButton} width={285} height={285} alt="oops"/>
+              <Image src={About} width={285} height={285} alt="oops"/>
             </div>
             <span>ABOUT</span>
           </Link>
